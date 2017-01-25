910adc2d279696e01d7c516d55e24027b3dd38cd
silvestrevivo <silvestrevivomillan@gmail.com>
2017-01-26 00:19:02 +0100

    add files

---
+++ js/row-eq-height.js
@@ -0,0 +1,21 @@
+function equalHeight(){
+	if(jQuery(window).width() > 768	){
+		jQuery('.row-eq-height').each(function(){
+			var height = [];
+			jQuery(this).children().each(function(i){
+				height[i] = jQuery(this).innerHeight();
+			});
+			Array.max = function( array ){
+			    return Math.max.apply( Math, array );
+			};
+			var maxHeight = Array.max(height);
+			jQuery(this).children().innerHeight(maxHeight);
+		});
+	}
+}
+//Resize
+jQuery(document).ready(equalHeight);
+jQuery(window).resize(function(){
+	jQuery('.row-eq-height').children().css('height', 'auto');
+	equalHeight();
+});
